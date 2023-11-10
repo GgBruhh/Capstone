69c18d610fa41e7eed3a5dd67bc68365c624fecc
Camryn Scott <camryn1242@yahoo.com>
2023-11-10 09:58:25 -0500

    added description page and backdoors incase there was no data passed in

--- src/ShowDescription.js
+++ src/ShowDescription.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import {useParams} from 'react-router-dom';
 import AnilistApi from "./Api";
 
@@ -6,19 +6,81 @@ import AnilistApi from "./Api";
 function ShowDescription(){
 
     const {id} = useParams();
+    
+    const [data, setData] = useState(null);
 
-    async function retrieveData(id){
-        const result = await AnilistApi.searchId(id);
-        console.log(result)
-        return result;
+    useEffect(() => {
+        // Define an asynchronous function inside useEffect to handle the data retrieval
+        const fetchData = async () => {
+            try {
+                const result = await AnilistApi.searchId(id);
+                setData(result);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                // Handle errors if needed
+            }
+        };
+
+        // Call the asynchronous function
+        fetchData();
+    }, [id]); // Ensure useEffect runs when 'id' changes
+
+    function checkData() {
+        if (data && data.data && data.data.Media) {
+            const show = data.data.Media;
+            console.log(show);
+            return show;
+        } else {
+            return null; // Return null instead of undefined for consistency
+        }
+    }
+    
+    function checkTitle(showData) {
+        if (showData && showData.title) {
+            const { english, romaji } = showData.title;
+    
+            if (english) {
+                return english;
+            } else if (romaji) {
+                return romaji;
+            }
+        }
+    
+        // Return a default value or handle the case when neither English nor Romaji title is available
+        return "Title Not Available";
+    }
+    
+    function checkImgSrc(showData) {
+        if (showData && showData.coverImage && showData.coverImage.extraLarge) {
+            const imgSrc = showData.coverImage.extraLarge;
+            return imgSrc;
+        }
+    
+        // Return a default value or handle incase there is no image route
+        return "Image Not Available";
     }
 
-    retrieveData(id)
+    function checkDescription(showData) {
+        if (showData && showData.description) {
+            const description = showData.description;
+            return description;
+        }
+    
+        // Return a default value or handle incase there is no description defined.
+        return "Description Not Available";
+    }
+    
+    const showData = checkData();
+    const title = checkTitle(showData);
+    const imgSrc = checkImgSrc(showData);
+    const description = checkDescription(showData);
+    
 
-    console.log(id)
     return(
         <div>
-            <h1>This is the Show Description</h1>
+            <h1>{title}</h1>
+            <img src={imgSrc}/>
+            <i>Description: {description}</i>
         </div>
     )
 }
